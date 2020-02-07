3c4127d328c5e10d31c94988510194f1e22fca24
Siterizer <siterizer@gmail.com>
2020-02-07 12:19:22 +0100

    Deleted lane with 'exlude' it's uselees now

--- src/main/java/io/games/simple/MainApplication.java
+++ src/main/java/io/games/simple/MainApplication.java
@@ -2,9 +2,8 @@ package io.games.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class MainApplication {
 	public static void main(String[] args) { SpringApplication.run(MainApplication.class, args); }
 }
