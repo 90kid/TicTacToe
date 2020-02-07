019eeba9198d3bd9d894bbcbd470d173c6190332
unknown <krzysztofmichalski987@gmail.com>
2020-02-07 22:17:38 +0100

    add react

---
+++ src/main/resources/static/js/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class App extends React.Component{
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <h1>Header</h1>
+                </header>
+                <article>
+                    <h1>Article</h1>
+                </article>
+                <footer>
+                    <h1>Footer</h1>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default App
